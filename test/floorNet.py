39386fd095dd3fab11d765c2f8be4460801a6628
tphanson <tphanson@kambria.io>
2020-07-23 15:55:57 +0700

    debug

--- test/floorNet.py
+++ test/floorNet.py
@@ -23,8 +23,8 @@ def infer():
         mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
         collision = np.zeros(mask.shape, dtype=np.uint8)
         cv.line(collision, (90, 90), (134, 90), (0, 0, 255), 15)
-        print(collision/255)
-        cv.addWeighted(mask, 0.5, img, 0.5, 0, img)
+        collision = collision/255
+        cv.addWeighted(mask, 0.5, collision, 0.5, 0, collision)
 
         rosimg.apush(collision)
 
