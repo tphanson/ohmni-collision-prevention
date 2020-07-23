eac128aebbf39da3d2cc7568c33b2504c4bd5efd
tphanson <tphanson@kambria.io>
2020-07-23 16:14:09 +0700

    debug

--- test/floorNet.py
+++ test/floorNet.py
@@ -20,10 +20,11 @@ def infer():
         _, frame = camera.read()
         _, mask = floorNet.predict(frame)
         # Visualize
+        mask[90:110][90:134] = mask[90:110][90:134] + 0.5
         mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR) * 255
-        collision = np.zeros(mask.shape, dtype=np.float32)
-        cv.line(collision, (90, 100), (134, 100), (0, 0, 255), 20)
-        cv.addWeighted(mask, 0.5, collision, 0.5, 0, mask)
+        # collision = np.zeros(mask.shape, dtype=np.float32)
+        # cv.line(collision, (90, 100), (134, 100), (0, 0, 255), 20)
+        # cv.addWeighted(mask, 0.5, collision, 0.5, 0, mask)
 
         rosimg.apush(mask)
 
