f5a9761b206e198a81c1e71a21505df927cf0f3d
tphanson <tphanson@kambria.io>
2020-07-23 13:27:01 +0700

    debug

--- test/floorNet.py
+++ test/floorNet.py
@@ -2,13 +2,15 @@ import time
 import cv2 as cv
 import numpy as np
 
-from utils import camera
+from utils import camera, ros
 from src.floorNet import FloorNet
 
 
 def infer(server):
-    # Image source
+    # Init modules
     floorNet = FloorNet()
+    rosimg = ros.ROSImage()
+    rosimg.client.run()
     # Prediction
     while True:
         start = time.time()
@@ -20,16 +22,10 @@ def infer(server):
         # Visualize
         mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
         cv.addWeighted(mask, 0.5, img, 0.5, 0, img)
-        cv.imshow('Camera', img)
+        rosimg.apush(img)
 
         # Calculate frames per second (FPS)
         end = time.time()
         print('Total estimated time: {:.4f}'.format(end-start))
         fps = 1/(end-start)
         print("FPS: {:.1f}".format(fps))
-
-        if cv.waitKey(10) & 0xFF == ord('q'):
-            break
-    # Clear resources
-    cv.destroyAllWindows()
-    cam.terminate()
