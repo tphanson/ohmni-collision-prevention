a32c9a73ecfa5c5e8f49a1fbdc58edffb736330f
tphanson <tphanson@kambria.io>
2020-07-23 15:33:44 +0700

    debug

--- test/floorNet.py
+++ test/floorNet.py
@@ -21,7 +21,7 @@ def infer():
         # Visualize
         mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
         cv.addWeighted(mask, 0.5, img, 0.5, 0, img)
-        rosimg.apush(mask*255)
+        rosimg.apush(img*255)
 
         # Calculate frames per second (FPS)
         end = time.time()
