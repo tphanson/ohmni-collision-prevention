d2f19dc391dff83769da6e646d4288f1785da14a
tphanson <tphanson@kambria.io>
2020-08-04 15:14:00 +0700

    debug

--- test/datacollector.py
+++ test/datacollector.py
@@ -3,7 +3,7 @@ import cv2 as cv
 from utils import ros
 
 
-def client_collect():
+def host_collect():
     rosimg = ros.ROSImage()
     talker = rosimg.gen_talker('/ds/nav_cam/compressed')
     camera = cv.VideoCapture(1)
@@ -13,10 +13,10 @@ def client_collect():
         time.sleep(5)
 
 
-def host_collect():
+def client_collect():
     rosimg = ros.ROSImage(host='192.168.123.53')
     listener = rosimg.gen_listener('/ds/nav_cam/compressed')
     _, img = listener.get()
     cv.imshow('Video', img)
     if cv.waitKey(10) & 0xFF == ord('q'):
-            break
+        break
