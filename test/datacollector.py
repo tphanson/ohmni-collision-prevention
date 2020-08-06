c97f4c73b089d7be86034d951991b02da4c79e6c
tphanson <tphanson@kambria.io>
2020-08-06 16:49:11 +0700

    debug

--- test/datacollector.py
+++ test/datacollector.py
@@ -1,11 +1,16 @@
 import time
 import cv2 as cv
 
+# It may be changed with other devices
+USB_PATH = '/mnt/media_rw/3115-7E1B/ds/'
+
 
 def collect():
     camera = cv.VideoCapture(2)
+    count = 0
     while True:
-        ok, frame = camera.read()
+        count += 1
+        ok, img = camera.read()
         if ok:
-            print(frame.shape)
+            cv.imwrite(USB_PATH+str(count)+'.jpg', img)
         time.sleep(5)
