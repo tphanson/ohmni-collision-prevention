02c27bdf0e3c9b03da278aa6e6f94862e5d18000
tphanson <tphanson@kambria.io>
2020-08-18 16:28:14 +0700

    collect ds

--- test/datacollector.py
+++ test/datacollector.py
@@ -15,13 +15,12 @@ def calibrate():
     camera = cv.VideoCapture(1)
     count = 0
     while True:
+        time.sleep(5)  # Limit at 10Hz
         count += 1
         ok, img = camera.read()
         print("================== Image:", count, ok)
         if ok:
             talker.push(img)
-        # Limit at 10Hz
-        time.sleep(5)
 
 
 def collect():
