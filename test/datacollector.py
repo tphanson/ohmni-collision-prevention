22bd58f6aa7954e2834575934559089cc3e521dd
tphanson <tphanson@kambria.io>
2020-08-18 14:02:32 +0700

    calibrate camera

--- test/datacollector.py
+++ test/datacollector.py
@@ -8,7 +8,7 @@ DESTINATION = '../ds/'
 def calibrate():
     ros = ROSImage()
     talker = ros.gen_talker('/ds_calib/image/compressed')
-    camera = cv.VideoCapture(1)
+    camera = cv.VideoCapture(2)
     count = 0
     while True:
         count += 1
