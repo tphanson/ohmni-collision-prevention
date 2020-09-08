45ec59b66d9bd8a7bc705996ab2bbc1013002e40
tphanson <tphanson@kambria.io>
2020-09-08 16:10:29 +0700

    test odo

--- test/floorNet.py
+++ test/floorNet.py
@@ -43,7 +43,7 @@ def infer(botshell, debug=False):
         confidence = collision/area
         if confidence > 0.2:
             print('Stop it, idiots!', confidence)
-            botshell.sendall(b'manual_move -500 500\n')
+            # botshell.sendall(b'manual_move -500 500\n')
         else:
             botshell.sendall(b'manual_move 0 0\n')
         # Visualize
