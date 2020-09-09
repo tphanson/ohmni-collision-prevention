2306733e8f00a53bf213ef02ae7cd794dc36a2da
tphanson <tphanson@kambria.io>
2020-09-09 16:04:29 +0700

    test odo

--- test/floorNet.py
+++ test/floorNet.py
@@ -36,7 +36,7 @@ def infer(botshell, debug=False):
         img, mask = floorNet.predict(frame)
         img = (img*127.5+127.5)/255
         # Detect collision
-        R = 1000 / (vleft-vright if vleft-vright != 0 else 0.001)
+        R = 1000 / (vleft+vright if vleft+vright != 0 else 0.001)
         driving_zone = odo.generate_driving_zone(R, np.pi)
         bool_mask = image.get_mask_by_polygon(img, driving_zone)
         confidence = np.sum(mask[bool_mask])/np.sum(bool_mask)
