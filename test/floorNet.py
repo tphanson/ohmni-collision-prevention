58084e71771de64db1c2db385c2a194712a6b6ff
tphanson <tphanson@kambria.io>
2020-09-10 11:08:56 +0700

    test odo

--- test/floorNet.py
+++ test/floorNet.py
@@ -62,6 +62,6 @@ def infer(botshell, debug=False):
         fps = 1/(end-start)
         print('Total estimated time: {:.4f}'.format(end-start))
         print("FPS: {:.1f}".format(fps))
-
+        time.sleep(0.5)
     talker.stop()
     rosimg.stop()
