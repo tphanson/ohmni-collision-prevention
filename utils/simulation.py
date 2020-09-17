c830d9820cac652731e8a97e2ba123c4f0249bc2
tphanson <tphanson@kambria.io>
2020-09-17 15:13:13 +0700

    simulation

--- utils/simulation.py
+++ utils/simulation.py
@@ -5,9 +5,7 @@ import numpy as np
 
 
 def _simMovement(botshell):
-    counter = 0
-    while counter < 10:
-        counter += 1
+    while True:
         botshell.sendall(b'manual_move 500 -500\n')
         time.sleep(1)
 
