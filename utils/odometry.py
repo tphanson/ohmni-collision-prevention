e79c88ff4f900bcd599f27b044016588779fe3fa
tphanson <tphanson@kambria.io>
2020-09-17 17:26:35 +0700

    simulation

--- utils/odometry.py
+++ utils/odometry.py
@@ -72,10 +72,10 @@ class Odometry:
         self.botshell.sendall(cmd)
 
     def avoid_obstacles(self):
-        signal = random.choice([(-0.05, -0.05), (0.05, -0.05)])
+        signal = random.choice([(-0.5, -0.5), (0.5, -0.5)])
         cmd = self._move_cmd(signal)
         counter = 0
-        while counter < 50:  # Tuen left in one second
+        while counter < 5:  # Tuen left in one second
             counter += 1
             self.botshell.sendall(cmd)
             time.sleep(0.01)
