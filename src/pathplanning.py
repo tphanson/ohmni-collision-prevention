a481ce1bd7656db607e28254fd2af2013c8dc878
tphanson <tphanson@kambria.io>
2020-10-05 14:59:55 +0700

    debug

--- src/pathplanning.py
+++ src/pathplanning.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 
 
-def create_map(mask, density=(8, 8)):
+def create_map(mask, density=(14, 14)):
     _map = cv.resize(mask, density)
     print(_map)
     return _map
