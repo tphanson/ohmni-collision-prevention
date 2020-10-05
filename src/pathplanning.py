de0a37ffc1775d1f64392e89aeb4d0ccb79a9248
tphanson <tphanson@kambria.io>
2020-10-05 15:12:24 +0700

    debug

--- src/pathplanning.py
+++ src/pathplanning.py
@@ -4,7 +4,9 @@ import numpy as np
 
 def draw_bitmap(mask, density=(14, 14)):
     bitmap = cv.resize(mask, density)
+    print(bitmap[6:7, 6:7])
     return np.ceil(bitmap)
+
 
 def a_star(bitmap):
     return None
