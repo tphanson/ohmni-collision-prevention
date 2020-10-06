a6432e0dd275ab2da9cfb58e3edce8c45ea53530
tphanson <tphanson@kambria.io>
2020-10-06 10:43:23 +0700

    debug

--- src/pathplanning.py
+++ src/pathplanning.py
@@ -70,7 +70,7 @@ class PathPlanning():
                 if not visited and not occupied:
                     histogram[y, x] = min(
                         histogram[y, x], current_value + self._distance(current_node, neighbour))
-        print(histogram)
+        print(histogram[detination[1], detination[0]])
         # Trace the path
         if not histogram[detination[1], detination[0]] < np.inf:
             return None
