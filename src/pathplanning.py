ba5a67924424f064d0a175135caff0731f75b1b4
tphanson <tphanson@kambria.io>
2020-10-06 10:00:23 +0700

    debug

--- src/pathplanning.py
+++ src/pathplanning.py
@@ -11,20 +11,20 @@ class PathPlanning():
         points = []
         for y in range(self.density[0]):
             for x in range(self.density[1]):
-                points.append([y, x])
+                points.append([x, y])
         return points
 
     def _gen_neighbours(self, point):
-        [y, x] = point
+        [x, y] = point
         neighbours = np.unique([
-            [max(0, y-1), max(0, x-1)],
-            [max(0, y-1), x],
-            [max(0, y-1), min(13, x+1)],
-            [y, max(0, x-1)],
-            [y, min(13, x+1)],
-            [min(13, y+1), max(0, x-1)],
-            [min(13, y+1), x],
-            [min(13, y+1), min(13, x+1)],
+            [max(0, x-1), max(0, y-1)],
+            [x, max(0, y-1)],
+            [min(13, x+1), max(0, y-1)],
+            [max(0, x-1), y],
+            [min(13, x+1), y],
+            [max(0, x-1), min(13, y+1)],
+            [x, min(13, y+1)],
+            [min(13, x+1), min(13, y+1)],
         ], axis=0)
         neighbours = (e.tolist()
                       for e in neighbours if np.any(np.not_equal(e, point)))
@@ -32,9 +32,9 @@ class PathPlanning():
 
     def _gen_neighbours_map(self):
         neighbour_map = {}
-        for [y, x] in self._gen_points():
-            key = '[{},{}]'.format(y, x)
-            neighbour_map[key] = self._gen_neighbours([y, x])
+        for [x, y] in self._gen_points():
+            key = '[{},{}]'.format(x, y)
+            neighbour_map[key] = self._gen_neighbours([x, y])
         return neighbour_map
 
     def draw_bitmap(self, mask):
@@ -46,22 +46,22 @@ class PathPlanning():
         return np.linalg.norm(np.array(source)-np.array(destination))
 
     def neighbours(self, point):
-        [y, x] = point
-        key = '[{},{}]'.format(y, x)
+        [x, y] = point
+        key = '[{},{}]'.format(x, y)
         return self.neighbour_map[key]
 
     def dijkstra(self, bitmap, source, detination):
         visited_nodes = []
         unvisited_nodes = [source]
         histogram = np.full(self.density, np.inf)
-        histogram[source[0], source[1]] = 0.
+        histogram[source[1], source[0]] = 0.
         # Compute distances
         while len(unvisited_nodes) > 0:
             current_node = unvisited_nodes.pop(0)
             visited_nodes.append(current_node)
             for neighbour in self.neighbours(current_node):
-                [y, x] = neighbour
-                current_value = histogram[current_node[0], current_node[1]]
+                [x, y] = neighbour
+                current_value = histogram[current_node[1], current_node[0]]
                 visited = neighbour in visited_nodes
                 waiting = neighbour in unvisited_nodes
                 occupied = bitmap[y, x] != 0
@@ -71,7 +71,7 @@ class PathPlanning():
                     histogram[y, x] = min(
                         histogram[y, x], current_value + self._distance(current_node, neighbour))
         # Trace the path
-        if not histogram[detination[0], detination[1]] < np.inf:
+        if not histogram[detination[1], detination[0]] < np.inf:
             return None
         trajectory = [detination]
         reached = False
