f68d3514255e7f46455b771655c6565d3e015dc3
tphanson <tphanson@kambria.io>
2020-10-06 11:06:04 +0700

    debug

--- utils/image.py
+++ utils/image.py
@@ -20,7 +20,7 @@ def draw_polygon(img, polygon):
 
 
 def draw_trajectory(img, trajectory):
-    for segment in zip(tuple(trajectory[:-1].tolist()), tuple(trajectory[1:].tolist())):
+    for segment in zip(trajectory[:-1].tolist(), trajectory[1:].tolist()):
         print(segment)
-        cv.line(img, segment[0], segment[1], COLOR_RED, 5)
+        cv.line(img, tuple(segment[0]), tuple(segment[1]), COLOR_RED, 5)
     return img
